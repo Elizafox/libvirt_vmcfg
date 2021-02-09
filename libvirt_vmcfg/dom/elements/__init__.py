f0f0ff8858507e99d19fdb6e28f2437b2955f4e3
Elizabeth Myers <elizabeth@interlinked.me>
2021-02-09 09:14:10 -0600

    osconfig: remove OSConfig class, it was very qemu-specific
    A different class will be needed for xen, etc.

--- libvirt_vmcfg/dom/elements/__init__.py
+++ libvirt_vmcfg/dom/elements/__init__.py
@@ -5,7 +5,6 @@ from libvirt_vmcfg.dom.elements.features import FeaturesSimple, X86Features
 from libvirt_vmcfg.dom.elements.memory import Memory
 from libvirt_vmcfg.dom.elements.metadata import Metadata
 from libvirt_vmcfg.dom.elements.name import Name
-from libvirt_vmcfg.dom.elements.osconfig import (VirtTypes, OSConfig,
-                                                 QemuOSConfig)
+from libvirt_vmcfg.dom.elements.osconfig import QemuOSConfig
 from libvirt_vmcfg.dom.elements.power_management import PowerManagement
 from libvirt_vmcfg.dom.elements.uuid import DomainUUID
--- libvirt_vmcfg/dom/elements/osconfig.py
+++ libvirt_vmcfg/dom/elements/osconfig.py
@@ -6,19 +6,13 @@ from lxml import etree
 from libvirt_vmcfg.dom import Element
 
 
-class VirtTypes(Enum):
-    HVM = "hvm"
-
-
-class OSConfig(Element):
+class QemuOSConfig(Element):
     unique: bool = True
 
     def __init__(self, arch: str, machine: str,
-                 type: VirtTypes, boot_dev_order:
-                                  Optional[Sequence[str]] = None):
+                 boot_dev_order: Optional[Sequence[str]] = None):
         self.arch = arch
         self.machine = machine
-        self.type = type
         if boot_dev_order is None:
             boot_dev_order = []
         self.boot_dev_order = boot_dev_order
@@ -27,26 +21,13 @@ class OSConfig(Element):
         os_tag = etree.SubElement(root, "os")
         type_tag = etree.SubElement(os_tag, "type", arch=self.arch,
                                     machine=self.machine)
-        type_tag.text = self.type.value
+        type_tag.text = "hvm"
 
         boot_tags = []
         for dev in self.boot_dev_order:
             boot_tags.append(etree.SubElement(os_tag, "boot", dev=dev))
 
         return [os_tag, type_tag] + boot_tags
-
-    def __repr__(self):
-        return (f"OSConfig(arch={self.arch!r}, machine={self.machine!r}, "
-                f"type={self.type}, boot_dev_order={self.boot_dev_order}")
-
-
-class QemuOSConfig(OSConfig):
-    unique: bool = True
-
-    def __init__(self, arch: str, machine: str,
-                 boot_dev_order: Optional[Sequence[str]] = None):
-        # HVM is the only option on QEMU/KVM
-        super().__init__(arch, machine, VirtTypes.HVM, boot_dev_order)
 
     def __repr__(self):
         return (f"QemuOSConfig(arch={self.arch!r}, machine={self.machine!r}, "
