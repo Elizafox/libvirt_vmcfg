19656f4ce7019bc3ca3db2d463a093b133b45962
Elizabeth Myers <elizabeth@interlinked.me>
2021-02-09 08:58:50 -0600

    name is gone, name this appropriately

--- libvirt_vmcfg/dom/elements/name.py
+++ libvirt_vmcfg/dom/elements/name.py
@@ -8,13 +8,13 @@ from libvirt_vmcfg.dom import Element
 class Name(Element):
     unique: bool = True
 
-    def __init__(self, vm_name: str):
-        self.vm_name = vm_name
+    def __init__(self, name: str):
+        self.name = name
 
     def attach_xml(self, root: etree._Element) -> Sequence[etree._Element]:
         name_tag = etree.SubElement(root, "name")
-        name_tag.text = str(self.vm_name)
+        name_tag.text = str(self.name)
         return [name_tag]
 
     def __repr__(self):
-        return f"Name({self.vm_name!r})"
+        return f"Name({self.name!r})"
