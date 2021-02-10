2b2e81a4b1ff0c39d2c063baaba09f4aecda1ce9
Elizabeth Myers <elizabeth@interlinked.me>
2021-02-10 02:12:55 -0600

    clock: enable more features of libvirt

--- libvirt_vmcfg/dom/elements/devices/clock.py
+++ libvirt_vmcfg/dom/elements/devices/clock.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import NamedTuple, Optional, Sequence
+from typing import Dict, NamedTuple, Optional, Sequence
 
 from lxml import etree
 
@@ -10,38 +10,48 @@ class TimerType(Enum):
     RTC = "rtc"
     PIT = "pit"
     HPET = "hpet"
+    KVMCLOCK = "kvmclock"
+    TSC = "tsc"
+    HYPERVCLOCK = "hypervclock"
+    ARMVTIMER = "armvtimer"
 
 
 class TickPolicy(Enum):
-    UNKNOWN = ""
     CATCHUP = "catchup"
     DELAY = "delay"
+    MERGE = "merge"
+    DISCARD = "discard"
 
 
 class OffsetType(Enum):
-    UNKNOWN = ""
     UTC = "utc"
+    LOCALTIME = "localtime"
+    TIMEZONE = "timezone"
+    VARIABLE = "variable"
 
 
 class TimerDefinition(NamedTuple):
     type: TimerType
     tickpolicy: Optional[TickPolicy]
     present: bool
+    args: Optional[Dict[str, str]]
 
 
 class Clock(Element):
     unique: bool = True
 
     def __init__(self, offset: OffsetType = OffsetType.UTC,
-                 timers: Optional[Sequence[TimerDefinition]] = None):
+                 timers: Optional[Sequence[TimerDefinition]] = None, *,
+                 offset_args: Optional[Dict[str, str]] = None):
         if timers is None:
             timers = (
-                TimerDefinition(TimerType.RTC, TickPolicy.CATCHUP, True),
-                TimerDefinition(TimerType.PIT, TickPolicy.DELAY, True),
-                TimerDefinition(TimerType.HPET, None, False),
+                TimerDefinition(TimerType.RTC, TickPolicy.CATCHUP, True, None),
+                TimerDefinition(TimerType.PIT, TickPolicy.DELAY, True, None),
+                TimerDefinition(TimerType.HPET, None, False, None),
             )
 
         self.offset = offset
+        self.offset_args = offset_args
         self.timers = timers
 
     def attach_xml(self, root: etree._Element) -> Sequence[etree._Element]:
@@ -49,14 +59,22 @@ class Clock(Element):
         if self.offset and self.offset.value:
             clock_tag.attrib["offset"] = self.offset.value
 
+        if self.offset_args:
+            {clock_tag.attrib[k]: v for k, v in self.offset_args.items()}
+
         # Attach subtags for timers
         for timer in self.timers:
             timer_tag = etree.SubElement(clock_tag, "timer",
                                          name=timer.type.value)
             if not timer.present:
                 timer_tag.attrib["present"] = "no"
-            elif timer.tickpolicy and timer.tickpolicy.value:
+                continue
+
+            if timer.tickpolicy and timer.tickpolicy.value:
                 timer_tag.attrib["tickpolicy"] = timer.tickpolicy.value
+
+            if timer.args:
+                {timer_tag.attrib[k]: v for k, v in timer.args.items()}
 
         return [clock_tag]
 
