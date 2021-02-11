7b8bd0de4d1a46ce67db1a7b917fa6a4b04217c9
Elizabeth Myers <elizabeth@interlinked.me>
2021-02-11 07:39:20 -0600

    Move gen_mac out of the interface class.

--- libvirt_vmcfg/dom/elements/devices/interface.py
+++ libvirt_vmcfg/dom/elements/devices/interface.py
@@ -6,22 +6,22 @@ from lxml import etree
 from libvirt_vmcfg.dom.elements.devices import Device
 
 
+def gen_mac() -> str:
+    seq = list(urandom(6))
+    # Clear group bit and set local only bit
+    seq[0] &= ~0x1
+    seq[0] |= 0x2
+    return ":".join(f"{octet:0{2}x}" for octet in seq)
+
+
 class BridgedInterface(Device):
     unique: bool = False
 
     def __init__(self, interface: str, mac: Optional[str] = None,
                  model: str = "virtio"):
         self.interface = interface
-        self.mac = self.gen_mac() if mac is None else mac
+        self.mac = gen_mac() if mac is None else mac
         self.model = model
-
-    @staticmethod
-    def gen_mac() -> str:
-        seq = list(urandom(6))
-        # Clear group bit and set local only bit
-        seq[0] &= ~0x1
-        seq[0] |= 0x2
-        return ":".join(f"{octet:0{2}x}" for octet in seq)
 
     def attach_xml(self, root: etree._Element) -> Sequence[etree._Element]:
         devices_tag = self.get_devices_tag(root)
