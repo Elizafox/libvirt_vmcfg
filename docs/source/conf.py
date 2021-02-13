cb4ca685df7b56b2a8f1b573e04232ed72e5043f
Elizabeth Myers <elizabeth@interlinked.me>
2021-02-13 06:53:34 -0600

    0.0.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "Elizabeth Myers"
 
 # The full version, including alpha/beta/rc tags
 version = "0.0"
-release = "0.0.2"
+release = "0.0.3"
 
 
 # -- General configuration ---------------------------------------------------
