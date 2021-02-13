8f0f822877903b879bbe87062ff0d53fe776b490
Elizabeth Myers <elizabeth@interlinked.me>
2021-02-13 06:55:13 -0600

    Increment version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "Elizabeth Myers"
 
 # The full version, including alpha/beta/rc tags
 version = "0.0"
-release = "0.0.3"
+release = "0.0.4"
 
 
 # -- General configuration ---------------------------------------------------
